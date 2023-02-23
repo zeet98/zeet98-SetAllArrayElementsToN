1681cf58463f6fa6f00e037075be9d76d64fbf6e
Zeshan Tawa <zeshan.tawa98@gmail.com>
2023-02-23 21:00:39 +0000

    Complete SetAllArrayToN assignment

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -23,6 +23,9 @@ public class Lab {
      * @return nothing, because of pass-by-reference, all changes will be reflected in the original array.
      */
     public void set(int[] arr, int n){
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = n;
+        }
 
     }
 }
